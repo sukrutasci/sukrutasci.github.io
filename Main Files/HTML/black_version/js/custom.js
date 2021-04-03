7e2759907ee698182a2669cc15a197a41f30f17c
Sukru Tasci <80707540+sukrutasci@users.noreply.github.com>
2021-04-03 22:54:10 +0200

    Update custom.js

--- Main Files/HTML/black_version/js/custom.js
+++ Main Files/HTML/black_version/js/custom.js
@@ -129,7 +129,6 @@ Assigned to: ThemeForest
 			enabled: true,
 			navigateByImgClick: true,
 			preload: [0,1]
-			tCounter: '%curr% / %total%'
 		},
 		image: {
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
